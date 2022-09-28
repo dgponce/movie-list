927d19b69e3403acd18378e944c040838e7aee6b
Diana Garcia <dg38828@gmail.com>
2022-09-28 17:21:34 -0500

    Add movie addition logic

---
+++ assets/scripts/objects.js
@@ -0,0 +1,27 @@
+const addMovieButton = document.getElementById('add-movie-btn');
+const searchButton = document.getElementById('search-btn');
+
+const movies = [];
+
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
+
+  if (title.trim() === '' || extraName.trim() === '' || extraValue.trim() === '') {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue
+    },
+    id: Math.random()
+  };
+
+  movies.push(newMovie);
+  console.log(newMovie);
+};
+
+addMovieButton.addEventListener('click', addMovieHandler);
